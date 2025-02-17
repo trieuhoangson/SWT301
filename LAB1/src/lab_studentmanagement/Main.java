050a7e74f565109174d7845a17eba3fd47b03e2e
quangdung2004 <dungnqhe186457@fpt.edu.vn>
2025-02-18 01:06:42 +0700

    sửa

--- LAB1/src/lab_studentmanagement/Main.java
+++ LAB1/src/lab_studentmanagement/Main.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
+        //kk
         ArrayList<Student> ls = new ArrayList<>();
         Management management = new Management();
         Validation validation = new Validation();
