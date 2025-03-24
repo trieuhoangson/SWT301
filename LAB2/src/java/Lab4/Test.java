7d3ff0fdd17cc2cad75d395fade72f93eb0ff3e1
Phạm Việt Hoàng <phamviethoangvhhy@gmail.com>
2025-03-24 20:44:05 +0700

    1

--- LAB2/src/java/Lab4/Test.java
+++ LAB2/src/java/Lab4/Test.java
@@ -4,6 +4,11 @@
  */
 package Lab4;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,15 +18,64 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class Test {
 
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<>();
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public void demo52() throws InterruptedException {
+        driver.get("http://localhost:8080/SWP/login.jsp");
+        driver.manage().window().setSize(new Dimension(784, 912));
+
+        Thread.sleep(1000);
+        driver.findElement(By.linkText("Register Now")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.name("username")).sendKeys("anh12");
+        Thread.sleep(1000);
+        driver.findElement(By.name("fullname")).sendKeys("adddd");
+        Thread.sleep(1000);
+        driver.findElement(By.name("email")).sendKeys("cuong@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.name("phone")).sendKeys("0122331322");
+        Thread.sleep(1000);
+        driver.findElement(By.name("address")).sendKeys("aaaaaa");
+        Thread.sleep(1000);
+        driver.findElement(By.name("dateOfBirth")).sendKeys("4-02-2004");
+        Thread.sleep(1000);
+        driver.findElement(By.id("gender")).sendKeys("Male");
+        Thread.sleep(1000);
+        driver.findElement(By.name("password")).sendKeys("Hoangvhhy1z@");
+        Thread.sleep(1000);
+        driver.findElement(By.name("confirm_password")).sendKeys("Hoangvhhy1z@");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".btn")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("user")).sendKeys("anh12");
+        Thread.sleep(1000);
+        driver.findElement(By.id("password")).sendKeys("Hoangvhhy1z@");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        Thread.sleep(1000);
+    }
+
     public static void main(String[] args) {
+        Test test = new Test();
+        test.setUp();
         try {
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-            Thread.sleep(1*3000);
-            driver.quit();
-        } catch (Exception e) {
+            test.demo52();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            test.tearDown();
         }
-
     }
 }
